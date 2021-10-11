f6282715ab0aca171a9971f0ae99e68c6efe6a19
Conrad Wood <cnw@conradwood.net>
2021-10-11 17:48:25 +0100

    framework version 9517

--- src/golang.conradwood.net/vendor/golang.conradwood.net/apis/deployminator/create_1.go
+++ src/golang.conradwood.net/vendor/golang.conradwood.net/apis/deployminator/create_1.go
@@ -1,6 +1,6 @@
 // client create: DeployminatorClient
 /* geninfo:
-   filename  : protos/golang.conradwood.net/apis/deployminator/deployminator.proto
+   filename  : golang.conradwood.net/apis/deployminator/deployminator.proto
    gopackage : golang.conradwood.net/apis/deployminator
    importname: ai_0
    varname   : client_DeployminatorClient_0
--- src/golang.conradwood.net/vendor/golang.conradwood.net/go-easyops/cmdline/myversion.go
+++ src/golang.conradwood.net/vendor/golang.conradwood.net/go-easyops/cmdline/myversion.go
@@ -3,11 +3,11 @@ package cmdline
 // this file is automatically updated by the build server
 
 const (
-	BUILD_NUMBER        = 9314         // replaceme
-	BUILD_DESCRIPTION   = "Build #9314 of go-easyops at 2021-09-29 10:41:08.935791326 +0100 BST m=+0.009357757 on host " //replaceme
-	BUILD_TIMESTAMP     = 1632908468         //replaceme
+	BUILD_NUMBER        = 9517         // replaceme
+	BUILD_DESCRIPTION   = "Build #9517 of go-easyops at 2021-10-06 16:36:22.675429454 +0100 BST m=+0.005807683 on host " //replaceme
+	BUILD_TIMESTAMP     = 1633534582         //replaceme
 	BUILD_REPOSITORY_ID = 59         // replaceme
 	BUILD_REPOSITORY    = "go-easyops" // replaceme
-	BUILD_COMMIT        = "228b3e3a00d531cd9cdde86f11287db37fe0b1c1" // replaceme
+	BUILD_COMMIT        = "f8acc2939357b3a9d4f90b4bb8f4746dff6f317d" // replaceme
 )
 
