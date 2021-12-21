444e33c93d20fdf41f62ee212aa6087f04849e91
Conrad Wood <cnw@conradwood.net>
2021-12-21 09:15:25 +0000

    .
    Change-Id: I8418cf34f22b27ad5597fe18e77f01fad70bae7b

--- tools/create_users.sh
+++ tools/create_users.sh
@@ -5,7 +5,8 @@ while [ $CT -le 80 ]; do
     echo $CT
     MU=deploy${CT}
     echo "User: ${MU}"
-    useradd -s /bin/false -g deployers -M ${MU}
+    useradd -s /bin/sh -g deployers -M ${MU}
     usermod -g deployers -d /srv/autodeployer/deployments/${MU} ${MU}
+    usermod -s /bin/sh ${MU}
     CT=$((CT+1))
 done
