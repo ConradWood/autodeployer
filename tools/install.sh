038f4ff76eebf517cf96069b26c644e52342f018
Conrad Wood <cnw@conradwood.net>
2022-09-20 15:50:06 +0100

    .
    Change-Id: Ie5f45791637afcd7bb94ff768ec6a680524730f6

--- tools/install.sh
+++ tools/install.sh
@@ -7,6 +7,7 @@ fi
 
 if [ ! -f /tmp/autodeployer-server ]; then
     wget -O /tmp/autodeployer-server https://www.conradwood.net/builds/downloads/artefactid/8/version/latest/dist/linux/amd64/autodeployer-server || exit 10
+    chmod 755 /tmp/autodeployer-server || exit 10
 fi
 ssh ${HOST} "sudo chown cnw /tmp/autodeployer-server  >/dev/null 2>&1 ; sudo chown cnw /usr/local/bin/autodeployer-server ; sudo rm /usr/local/bin/autodeployer-server"
 rsync -pvraz --progress /tmp/autodeployer-server ${HOST}:/tmp/
