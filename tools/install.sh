aaf8b61857ef284d9b9e50fdd4e6a75bf3bf7b11
Conrad Wood <cnw@conradwood.net>
2023-01-19 13:45:17 +0000

    automatic commit
    Change-Id: I8a9a71c7e5668299b06646c7b37857cfc1fd820f

--- tools/install.sh
+++ tools/install.sh
@@ -11,6 +11,7 @@ if [ ! -f /tmp/autodeployer-server ]; then
 fi
 ssh ${HOST} "sudo chown cnw /tmp/autodeployer-server  >/dev/null 2>&1 ; sudo chown cnw /usr/local/bin/autodeployer-server ; sudo rm /usr/local/bin/autodeployer-server"
 rsync -pvraz --progress /tmp/autodeployer-server ${HOST}:/tmp/
+autodeployer-client -server=${HOST} -stop
 echo "installing autodeployer..."
 ssh ${HOST} "sudo cp /tmp/autodeployer-server /usr/local/bin ; sudo chown cnw /usr/local/bin/autodeployer-server ; sudo chmod 755 /usr/local/bin/autodeployer-server ;  sudo systemctl restart autodeployer"
 echo "done"
