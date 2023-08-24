37eee9ed6e90aef2be8bae4c45b011f84e99a03e
Conrad Wood <cnw@conradwood.net>
2023-08-24 16:12:37 +0100

    automatic commit
    Change-Id: I8f35a4c958bfcafcb9bd9295c30fcb5181c56df3

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo You probably rather want to install autodeployer2.
+exit 10
 BINARY=~/devel/admintools/install_autodeployer.sh
 echo "Moved to admintools repository. executing ${BINARY} for your convenience"
 exec ${BINARY} $@
