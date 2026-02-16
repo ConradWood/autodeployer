6224ff72e7b4d60dd8e27f55db33dfb467f55f89
Conrad Wood <cnw@conradwood.net>
2026-02-16 14:33:19 +0000

    better logging
    Change-Id: I229ddb2468017b5575b1245f4b38703e9e0fb502

--- src/golang.conradwood.net/deploymonkey/server/app_changed.go
+++ src/golang.conradwood.net/deploymonkey/server/app_changed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 
 	common "golang.conradwood.net/apis/common"
 	"golang.conradwood.net/apis/deploymonkey"
@@ -10,7 +11,12 @@ import (
 
 func (e *DeployMonkey) AppChanged(ctx context.Context, req *deploymonkey.AppChangeRequest) (*common.Void, error) {
 	appdef := req.AppRef.AppDef
-	appname := fmt.Sprintf("[%s appdef-id=%d Binary=%s buildid=%d, deplid=%s, artefactid=%d]", req.DeploymentID, appdef.ID, appdef.Binary, appdef.BuildID, appdef.DeploymentID, appdef.ArtefactID)
+	ipc_s := "ipc not supported"
+	if req.IPCSupported {
+		ipc_s = "IPCName=" + req.IPCName
+	}
+	bn := filepath.Base(appdef.Binary)
+	appname := fmt.Sprintf("[appchange %s appdef-id=%d Binary=%s buildid=%d, deplid=%s, artefactid=%d ipc=%s]", req.DeploymentID, appdef.ID, bn, appdef.BuildID, appdef.DeploymentID, appdef.ArtefactID, ipc_s)
 	fmt.Printf("Autodeployer reports app %s has changed to %v (undeplyrequest=%v) after %d seconds runtime\n", appname, req.Status, req.UndeployRequestReceived, req.RuntimeSeconds)
 	return &common.Void{}, nil
 }
