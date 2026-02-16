0dff3f89792f1ff608d045107b8a251b58b92fa5
Conrad Wood <cnw@conradwood.net>
2026-02-16 14:50:37 +0000

    synced

--- src/golang.conradwood.net/deploymonkey/server/app_changed.go
+++ src/golang.conradwood.net/deploymonkey/server/app_changed.go
@@ -16,7 +16,10 @@ func (e *DeployMonkey) AppChanged(ctx context.Context, req *deploymonkey.AppChan
 		ipc_s = "IPCName=" + req.IPCName
 	}
 	bn := filepath.Base(appdef.Binary)
-	appname := fmt.Sprintf("[appchange deplid=%s appdef-id=%d Binary=%s buildid=%d, appdef-deplid=%s, artefactid=%d ipc=%s]", req.DeploymentID, appdef.ID, bn, appdef.BuildID, appdef.DeploymentID, appdef.ArtefactID, ipc_s)
-	fmt.Printf("Autodeployer reports app %s has changed to %v (undeplyrequest=%v) after %d seconds runtime\n", appname, req.Status, req.UndeployRequestReceived, req.RuntimeSeconds)
+	prefix := fmt.Sprintf("[appchange binary=%s deplid=%s appdef-deplid=%s] ", bn, req.DeploymentID, appdef.DeploymentID)
+	//	appname := fmt.Sprintf("[appchange deplid=%s appdef-id=%d Binary=%s buildid=%d, appdef-deplid=%s, artefactid=%d ipc=%s]", req.DeploymentID, appdef.ID, bn, appdef.BuildID, appdef.DeploymentID, appdef.ArtefactID, ipc_s)
+	fmt.Printf(prefix+"Autodeployer reports status has changed to %v after %d seconds runtime\n", req.Status, req.RuntimeSeconds)
+	fmt.Printf(prefix+"   undeployrequest received=%v\n", req.UndeployRequestReceived)
+	fmt.Printf(prefix+"   ipc=%s, buildid=%d, health=%v\n", ipc_s, appdef.BuildID, req.Health)
 	return &common.Void{}, nil
 }
